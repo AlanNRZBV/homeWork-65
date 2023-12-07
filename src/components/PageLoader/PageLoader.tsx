7b1fc77789063d0e41b8d393de6ebb26d648ae65
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-12-07 14:48:35 +0600

    style: added styles for PageLoader

--- src/components/PageLoader/PageLoader.tsx
+++ src/components/PageLoader/PageLoader.tsx
@@ -1,18 +1,25 @@
-import { FC } from "react";
-import { IPageLoader } from "../../types";
+import { FC } from 'react';
+import { IPageLoader } from '../../types';
+import { motion } from 'framer-motion';
 
-const PageLoader: FC<IPageLoader> = ({content,isHome}) => {
+const PageLoader: FC<IPageLoader> = ({ content, isHome }) => {
   return (
-    <>
+    <div className="d-flex justify-content-center align-items-center pt-5">
       {isHome ? (
-        <h2>Home page content loaded</h2>
-      ):(
-    <div className="border border-1 rounded-3 shadow-sm">
-      <span className="text text-black">{content.title}</span>
-      <p>{content.content}</p>
-    </div>
+        <motion.h1 animate={{ y: 0, opacity: 1 }} initial={{ y: -100, opacity: 0 }} className="">
+          Home page content loaded
+        </motion.h1>
+      ) : (
+        <motion.div
+          animate={{ y: 0, opacity: 1 }}
+          initial={{ y: -100, opacity: 0 }}
+          className="border border-1 rounded-3 shadow-sm py-3 px-3"
+        >
+          <h3 className="text text-black">{content.title}</h3>
+          <p className="text text-secondary">{content.content}</p>
+        </motion.div>
       )}
-    </>
+    </div>
   );
 };
 
