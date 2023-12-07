7ece9b1d503931bbb5eed00f9e54a7c0560ee411
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-12-07 17:01:55 +0600

    prettier: format project

--- src/components/CustomNavLink/CustomNavLink.tsx
+++ src/components/CustomNavLink/CustomNavLink.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
-import { NavLink } from "react-router-dom";
-import { ICustomNavLink } from "../../types";
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ICustomNavLink } from '../../types';
 
-const CustomNavLink: FC<ICustomNavLink> = ({link}) => {
-  const innerCapitalized = link.toUpperCase()
+const CustomNavLink: FC<ICustomNavLink> = ({ link }) => {
+  const innerCapitalized = link.toUpperCase();
   return (
     <NavLink className="nav-link" to={`pages/${link}`}>
       {innerCapitalized}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,45 +1,43 @@
-import React from "react";
+import React from 'react';
 
 export interface IPageLoader {
-  content: IPageContent
-  isHome: boolean
+  content: IPageContent;
+  isHome: boolean;
 }
 
-export interface IPageContent{
-  title: string,
-  content: string
+export interface IPageContent {
+  title: string;
+  content: string;
 }
 
 export interface IPagesItem {
-  id: string,
-  title: string,
-  content: string
+  id: string;
+  title: string;
+  content: string;
 }
 
 export interface IPagesList {
-  [id:string]: IPageContent
+  [id: string]: IPageContent;
 }
 
-export  interface IContentTool {
-  pages: IPagesItem[]
-  onSelect: (selected: IOptions | null)=>void
-  onSubmit: ()=>void,
-  onChange: (e: React.ChangeEvent<HTMLInputElement>)=>void
-  pageData: IPageContent
-  reset: ()=>void
+export interface IContentTool {
+  pages: IPagesItem[];
+  onSelect: (selected: IOptions | null) => void;
+  onSubmit: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  pageData: IPageContent;
+  reset: () => void;
 }
-export interface IOptions{
-  value: string,
-  label: string
+export interface IOptions {
+  value: string;
+  label: string;
 }
 
 export interface ICustomNavLink {
-  link: string
+  link: string;
 }
 
 export interface IToggle {
-  nav: boolean,
-  pageItem: boolean
+  nav: boolean;
+  pageItem: boolean;
 }
-
-
